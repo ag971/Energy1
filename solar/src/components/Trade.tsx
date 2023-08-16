c2ee50a6c86de7e89ddbc697f3ce64fbedde9416
ag971 <135845341+ag971@users.noreply.github.com>
2023-08-17 00:29:11 +0100

    Update Trade.tsx

--- solar/src/components/Trade.tsx
+++ solar/src/components/Trade.tsx
@@ -1,58 +1,80 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { toByteString, hash160, bsv, TestWallet, DefaultProvider, ScryptProvider, SensiletSigner, findSig, MethodCallOptions, PubKey, StatefulNext } from 'scrypt-ts';
 import { EnergyTradingEscrow } from '../contracts/energy';
-import { toByteString, hash160, bsv, ScryptProvider, SensiletSigner, PubKey, Sig, findSig, MethodCallOptions, Signer} from 'scrypt-ts';
 
 function Trade() {
-    const [contract, setContract] = useState<EnergyTradingEscrow | null>(null);
-    const [energyAmount, setEnergyAmount] = useState<bigint>(0n);
-    const [energyPrice, setEnergyPrice] = useState<bigint>(0n);
-    const [availableEnergies, setAvailableEnergies] = useState<{ amount: bigint, price: bigint }[]>([]);
-    const [sellerPubKey, setSellerPubKey] = useState<bsv.PublicKey | null>(null);
-    const [buyerPubKey, setBuyerPubKey] = useState<bsv.PublicKey| null>(null);
 
-    const handleAddAndDeploy = async () => {
+    const [contract, setContract] = useState<EnergyTradingEscrow | null>(null);
+    const [buyerPubKey, setBuyerPubKey] = useState<bsv.PublicKey| null>(null);
+    const [energyAmount, setEnergyAmount] = useState<bigint>(0n);
+    const [sellerPubKey, setSellerPubKey] = useState<bsv.PublicKey| null>(null);
+    const [availableEnergyList, setAvailableEnergyList] = useState<Array<{price: bigint, energy: bigint}>>([]);
+    const [unitPrice, setUnitPrice] = useState<bigint>(0n);
+
+
+    const deployContract = async () => {
         const provider = new ScryptProvider();
         const signer = new SensiletSigner(provider);
+        
+        const seller = await signer.getDefaultPubKey()
+        const buyer = await signer.getDefaultPubKey()
 
-        const seller = await signer.getDefaultPubKey();
-        setSellerPubKey(seller);
+        setSellerPubKey(seller)
+        setBuyerPubKey(buyer)
 
-        const buyer = await signer.getDefaultPubKey();  // Simulating buyer public key here
-
-        const instance = new EnergyTradingEscrow(hash160(seller.toHex()), hash160(buyer.toHex()), energyPrice);
+        const instance = new EnergyTradingEscrow(hash160(seller.toHex()), hash160(buyer.toHex()), 1n);
         await instance.connect(signer);
 
         const deployTx = await instance.deploy();
         console.log(`EnergyTradingEscrow contract deployed: ${deployTx.id}`);
 
         setContract(instance);
-        setAvailableEnergies([...availableEnergies, { amount: energyAmount, price: energyPrice }]);
-        setEnergyAmount(0n);
-        setEnergyPrice(0n);
+    };
+    const depositEnergy = async () => {
+        try {
+            if (!contract || !sellerPubKey) {
+                console.error('Contract instance or seller public key is not available.');
+
+                setAvailableEnergyList(prevList => [...prevList, { price: unitPrice, energy: energyAmount }]);
+                return;
+            }
+        
+
+            const next = contract.next();
+            next.energy += energyAmount;
+
+            const result = await contract.methods.depositEnergy(
+                (sigResps) => findSig(sigResps, sellerPubKey),
+                PubKey(sellerPubKey.toHex()),
+                energyAmount,
+                {
+                    pubKeyOrAddrToSign: sellerPubKey,
+                    next: {
+                        instance: next,
+                        balance: contract.balance
+                    }
+                } as MethodCallOptions<EnergyTradingEscrow>
+            );
+
+            console.log('Deposit energy call txid:', result.tx.id);
+        } catch (error) {
+            console.error('Error depositing energy:', error);
+        }
     };
 
-    const handleBuyEnergy = async (energy) => {
+    const buyEnergy = async () => {
         try {
-            if (!contract) {
-                console.error('Contract instance is not available.');
+            if (!contract || !buyerPubKey) {
+                console.error('Contract instance or buyer public key is not available.');
                 return;
             }
-    
-            if (!buyerPubKey) {
-                console.error('Buyer pub key is not yet available.');
-                return;
-            }
-    
-            const provider = new ScryptProvider();
-            const signer = new SensiletSigner(provider);
-            const buyerSig = await signTx(buyerPubKey);
-    
+
             const next = contract.next();
-            next.energy -= energy.amount;
-    
+            next.energy -= energyAmount;
+
             const result = await contract.methods.buyEnergy(
                 PubKey(buyerPubKey.toHex()),
-                buyerSig,
+                (sigResps) => findSig(sigResps, buyerPubKey),
                 {
                     pubKeyOrAddrToSign: buyerPubKey,
                     next: {
@@ -61,46 +83,33 @@ function Trade() {
                     }
                 } as MethodCallOptions<EnergyTradingEscrow>
             );
-    
-            console.log('Buy energy call txid:', result.tx.id);
-    
-            const newAvailableEnergies = availableEnergies.filter(e => e.amount !== energy.amount && e.price !== energy.price);
-            setAvailableEnergies(newAvailableEnergies);
-    
+
+            console.log('Buy energy result:', result.tx.id);
         } catch (error) {
             console.error('Error buying energy:', error);
         }
     };
-    
+
     
 
     return (
         <div>
+            <button onClick={deployContract}>Deploy Contract</button>
             <div>
-                <h3>Add Energy</h3>
-                <div>
-                    <input type="number" placeholder="Energy Amount" value={energyAmount.toString()} onChange={(e) => setEnergyAmount(BigInt(e.target.value))} />
-                    Energy Amount in kW
-                </div>
-                <div>
-                    <input type="number" placeholder="Energy Price" value={energyPrice.toString()} onChange={(e) => setEnergyPrice(BigInt(e.target.value))} />
-                    Energy Price in BSV
-                </div>
-                <button onClick={handleAddAndDeploy}>Sell Energy</button>
+                <input 
+                    type="number" 
+                    placeholder="Energy Amount" 
+                    value={energyAmount.toString()} 
+                    onChange={(e) => setEnergyAmount(BigInt(e.target.value))} 
+                />
+                <button onClick={depositEnergy}>Deposit Energy</button>
             </div>
             <div>
-                <h3>Available List of Energy</h3>
-                {availableEnergies.map((energy, index) => (
-                    <div key={index} style={{ border: "1px solid gray", padding: "10px", margin: "10px 0" }}>
-                        <strong>{index + 1}.</strong>
-                        <p>Energy: {energy.amount.toString()} kW</p>
-                        <p>Price: BSV{energy.price.toString()}</p>
-                        <button onClick={() => handleBuyEnergy(energy)}>Buy Energy</button>
-                    </div>
-                ))}
+                <button onClick={buyEnergy}>Buy Energy</button>
             </div>
+            {/* Add other UI elements for other contract interactions if needed */}
         </div>
-    );
+    )
 }
 
 export default Trade;
