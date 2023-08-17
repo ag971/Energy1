54220df22544d1d2e5205814ab552de49b2e25d8
ag971 <135845341+ag971@users.noreply.github.com>
2023-08-18 00:52:33 +0100

    Update3 Trade.tsx

--- solar/src/components/Trade.tsx
+++ solar/src/components/Trade.tsx
@@ -1,176 +1,122 @@
-import React, { useState } from 'react';
-import { EnergyTradingEscrow } from '../contracts/energy'; // Assuming you import your contract here
-import { hash160, ScryptProvider, SensiletSigner, PubKey } from 'scrypt-ts';
-import bsv from 'bsv';
-import './Trade.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { toByteString, hash160, bsv, ScryptProvider, SensiletSigner, findSig, MethodCallOptions, PubKey,Sig,ContractTransaction,Utils } from 'scrypt-ts';
+import { EnergyTradingEscrow } from '../contracts/energy';
 
 function Trade() {
     const [contract, setContract] = useState<EnergyTradingEscrow | null>(null);
+    const [buyerPubKey, setBuyerPubKey] = useState<bsv.PublicKey | null>(null);
     const [energyAmount, setEnergyAmount] = useState<bigint>(0n);
-    const [energyPrice, setEnergyPrice] = useState<bigint>(0n);
-    const [availableEnergies, setAvailableEnergies] = useState<{ amount: bigint, price: bigint }[]>([]);
     const [sellerPubKey, setSellerPubKey] = useState<bsv.PublicKey | null>(null);
-    const [energyAmountError, setEnergyAmountError] = useState<string | null>(null);
-    const [energyPriceError, setEnergyPriceError] = useState<string | null>(null);
-    const [energyUnit, setEnergyUnit] = useState("kW");
-    const [showAddConfirmation, setShowAddConfirmation] = useState(false);
-    const [showBuySuccess, setShowBuySuccess] = useState(false);
-    const [selectedEnergy, setSelectedEnergy] = useState<{ amount: bigint, price: bigint } | null>(null);
-    const [currentEnergyPrice, setCurrentEnergyPrice] = useState<number>(0);
-    const [usageHours, setUsageHours] = useState<number>(0);
-    const [estimatedCost, setEstimatedCost] = useState<number | null>(null);
+    
+    // Create a reference for SensiletSigner
+    const signerRef = useRef<SensiletSigner | null>(null);
 
-    const handleAddAndDeploy = async () => {
+    // Initialize SensiletSigner on component mount
+    useEffect(() => {
         const provider = new ScryptProvider();
         const signer = new SensiletSigner(provider);
+        
+        // Assign the SensiletSigner instance to the ref
+        signerRef.current = signer;
+    }, []);
 
-        const seller = await signer.getDefaultPubKey();
+    const deployContract = async () => {
+        if (!signerRef.current) return;
+
+        const seller = await signerRef.current.getDefaultPubKey();
+        const buyer = await signerRef.current.getDefaultPubKey();
+        
         setSellerPubKey(seller);
+        setBuyerPubKey(buyer);
 
-        const buyer = await signer.getDefaultPubKey();  // Simulating buyer public key here
+        const sellerPubKeyHash = hash160(seller.toHex());
+        const buyerPubKeyHash = hash160(buyer.toHex());
+        const unitPrice = 1n;
 
-        const instance = new EnergyTradingEscrow(hash160(seller.toHex()), hash160(buyer.toHex()), energyPrice);
-        await instance.connect(signer);
+        const instance = new EnergyTradingEscrow(sellerPubKeyHash, buyerPubKeyHash, unitPrice);
+        await instance.connect(signerRef.current);
 
         const deployTx = await instance.deploy();
         console.log(`EnergyTradingEscrow contract deployed: ${deployTx.id}`);
 
         setContract(instance);
-        setAvailableEnergies([...availableEnergies, { amount: energyAmount, price: energyPrice }]);
-        setEnergyAmount(0n);
-        setEnergyPrice(0n);
     };
 
-    const handleBuyEnergy = async (energy: { amount: bigint, price: bigint }) => {
-        try {
-            const provider = new ScryptProvider();
-            const signer = new SensiletSigner(provider);
-
-            const buyerPubKey = await signer.getDefaultPubKey();
-            const buyerSig = await signer.signMessage(buyerPubKey.toHex());
-
-            // Simulate a successful energy purchase
-            const updatedEnergies = availableEnergies.filter(e => e !== energy);
-            setAvailableEnergies(updatedEnergies);
-
-                        // Store selected energy values
-                        setSelectedEnergy(energy);
-
-            // Set showBuySuccess to true only after successful energy purchase
-            setShowBuySuccess(true);
-        } catch (error) {
-            console.error("Error simulating Sensilet signing:", error);
+    const depositEnergy = async () => {
+        if (!contract || !sellerPubKey || !signerRef.current) {
+            console.error('Contract instance or seller public key is not available.');
+            return;
         }
+
+        const next = contract.next();
+        next.energy += energyAmount;
+
+        const result = await contract.methods.depositEnergy(
+            (sigResps) => findSig(sigResps, sellerPubKey),
+            PubKey(sellerPubKey.toHex()),
+            energyAmount,
+            {
+                pubKeyOrAddrToSign: sellerPubKey,
+                next: {
+                    instance: next,
+                    balance: contract.balance
+                }
+            } as MethodCallOptions<EnergyTradingEscrow>
+        ); 
+
+        console.log('Deposit energy call txid:', result.tx.id);
     };
 
-    const handleCalculateCost = () => {
-        // Assuming energy price is in pence per kW-hour
-        const energyPricePence = currentEnergyPrice * 100;
-        const costPence = (usageHours * energyPricePence) / 100;
-        setEstimatedCost(costPence);
+    const buyEnergy = async () => {
+        if (!contract || !buyerPubKey || !signerRef.current) {
+            console.error('Contract instance or buyer public key is not available.');
+            return;
+        }
+
+        contract.bindTxBuilder('buyEnergy', EnergyTradingEscrow.buyTxBuilder);
+
+        const next = contract.next();
+        next.energy -= energyAmount;
+
+        const result = await contract.methods.buyEnergy(
+            PubKey(buyerPubKey.toHex()),
+            (sigResps) => findSig(sigResps, buyerPubKey),
+            {
+                changeAddress: await signerRef.current.getDefaultAddress(),
+                pubKeyOrAddrToSign: buyerPubKey,
+                next: {
+                    instance: next,
+                    balance: contract.balance
+                }
+            } as MethodCallOptions<EnergyTradingEscrow>
+        );
+
+        console.log('Buy energy result:', result.tx.id);
     };
-    
+
     return (
-        <div>
+        <div style={{ padding: '20px' }}>
+            <h2>Energy Trading</h2>
+            <div style={{ marginBottom: '20px' }}>
+                <button onClick={deployContract}>Start</button>
+            </div>
+            <div style={{ marginBottom: '20px' }}>
+                <label>
+                    <span>Enter Energy Amount: </span>
+                    <input 
+                        type="number" 
+                        placeholder="Energy Amount" 
+                        value={energyAmount.toString()} 
+                        onChange={(e) => setEnergyAmount(BigInt(e.target.value))} 
+                    />
+                </label>
+                <button style={{ marginLeft: '10px' }} onClick={depositEnergy}>Deposit Energy</button>
+            </div>
             <div>
-                <h3>Add Energy</h3>
-                <div>
-                    <input
-                        type="number"
-                        placeholder="Energy Amount"
-                        value={energyAmount.toString()}
-                        onChange={(e) => {
-                            setEnergyAmount(BigInt(e.target.value));
-                            setEnergyAmountError(null);
-                        }}
-                    />
-                    {energyAmountError && <div className="error">{energyAmountError}</div>}
-                    <select value={energyUnit} onChange={(e) => setEnergyUnit(e.target.value)}>
-                        <option value="kW">kW</option>
-                        <option value="MW">MW</option>
-                        <option value="GW">GW</option>
-                    </select>
-                </div>
-                <div>
-            {/* ... Other JSX ... */}
-            <div>
-                <h3>Energy Consumption Calculator</h3>
-                <div>
-                    <label>Current Energy Price (£/kWh): </label>
-                    <input
-                        type="number"
-                        value={currentEnergyPrice}
-                        onChange={(e) => setCurrentEnergyPrice(Number(e.target.value))}
-                    />
-                </div>
-                <div>
-                    <label>Hours of Usage: </label>
-                    <input
-                        type="number"
-                        value={usageHours}
-                        onChange={(e) => setUsageHours(Number(e.target.value))}
-                    />
-                </div>
-                <div>
-                    <button onClick={handleCalculateCost}>Calculate Cost</button>
-                </div>
-                {estimatedCost !== null && (
-                    <p>Estimated Cost: £{estimatedCost / 100}</p>
-                )}
+                <button onClick={buyEnergy}>Buy Energy</button>
             </div>
         </div>
-                <div>
-                    <input
-                        type="number"
-                        placeholder="Energy Price"
-                        value={energyPrice.toString()}
-                        onChange={(e) => {
-                            setEnergyPrice(BigInt(e.target.value));
-                            setEnergyPriceError(null);
-                        }}
-                    />
-                    {energyPriceError && <div className="error">{energyPriceError}</div>}
-                    £
-                    <button onClick={() => setShowAddConfirmation(true)}>Add Energy</button>
-                </div>
-            </div>
-            <div>
-                <h3>Available List of Energy</h3>
-                {availableEnergies.map((energy, index) => (
-                    <div key={index} style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '10px', borderRadius: '5px' }}>
-                        <div>
-                            Energy: {energy.amount.toString()} kW - Price: £{energy.price.toString()}
-                        </div>
-                        <div>
-                            <button onClick={() => handleBuyEnergy(energy)}>Buy Energy</button>
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-            {showAddConfirmation && (
-                <div className="modal">
-                    <p>Are you sure you want to add into the marketplace?</p>
-                    <button onClick={() => {
-                        handleAddAndDeploy();
-                        setShowAddConfirmation(false);
-                    }}>Yes</button>
-                    <button onClick={() => setShowAddConfirmation(false)}>Cancel</button>
-                </div>
-            )}
-
-                {showBuySuccess && (
-                <div className="modal">
-                    <p>Congratulations! You have successfully bought {selectedEnergy?.amount.toString()} kW of energy at £{selectedEnergy?.price.toString()}.</p>
-                    <button onClick={() => {
-                        setSelectedEnergy(null); // Clear selected energy values
-                        setShowBuySuccess(false);
-                    }}>OK</button>
-                </div>
-            )}
-        </div>
-
-        
     );
 }
+
 export default Trade;
